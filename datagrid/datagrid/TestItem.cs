b840cc044ef9ff3264b68f5c27a2d28ac926e06e
saku <haruichi33333@gmail.com>
2019-03-05 22:12:52 +0900

    comboboxサンプル追加

--- datagrid/datagrid/TestItem.cs
+++ datagrid/datagrid/TestItem.cs
@@ -1,21 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Documents;
+using view_base;
 
 namespace datagrid
 {
-    class TestItem
+    class TestItem : view_base.ViewModel.ViewModelBase
     {
+        public ObservableCollection<Person> Persons
+        {
+            get { return this._persons; }
+            set
+            {
+                if (this._persons == value) return;
+                this._persons = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Person> _persons 
+            = new ObservableCollection<Person>();
+
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
         public string Gender { get; set; }
         public string Link { get; set; } = "https://www.google.com/";
+        public Person Person { get; set; }
     }
 }
 
--- datagrid/datagrid/ViewModel.cs
+++ datagrid/datagrid/ViewModel.cs
@@ -12,18 +12,27 @@ namespace datagrid
 {
     class ViewModel
     {
+        public ObservableCollection<Person> Persons { get; set; }
+        = new ObservableCollection<Person>();
         // コンストラクタ
         public ViewModel()
         {
             this.DoubleClickCommand = new v.DelegateCommand(this.MyDoubleClickCommand, x => true);
             this.CopyCommand = new v.DelegateCommand(this.CopyExecute, () => true);
             this.PasteCommand = new v.DelegateCommand(this.PasteExecute, () => true);
+            this.AddCommand = new v.DelegateCommand(this.AddExecute, () => true);
+            this.ReplaceCommand = new v.DelegateCommand(this.ReplaceExecute, () => true);
                 
+            this.Persons.Add(new Person("aaa", 2));
+            this.Persons.Add(new Person("bbb", 2));
+            this.Persons.Add(new Person("ccc", 2));
+
             //
             myList = new ObservableCollection<TestItem>();
-            myList.Add( new TestItem { Id = 1, Name = "7of9", Age = 30, Gender = "Female" });
-            myList.Add( new TestItem { Id = 2, Name = "Janeway", Age = 50, Gender = "Female" });
-            myList.Add( new TestItem { Id = 3, Name = "Chakotay", Age = 40, Gender = "Female" });
+            myList.Add(new TestItem { Id = 1, Name = "7of9", Age = 30, Gender = "Female", Persons = this.Persons });
+            myList.Add(new TestItem { Id = 2, Name = "Janeway", Age = 50, Gender = "Female", Persons = this.Persons });
+            myList.Add(new TestItem { Id = 3, Name = "Chakotay", Age = 40, Gender = "Female", Persons = this.Persons });
+
         }
 
         public ObservableCollection<TestItem> myList { get; set; }
@@ -31,6 +40,9 @@ namespace datagrid
         public ICommand DoubleClickCommand { get; private set; }
         public ICommand CopyCommand { get; private set; }
         public ICommand PasteCommand { get; private set; }
+
+        public ICommand AddCommand { get; private set; }
+        public ICommand ReplaceCommand { get; private set; }
 
         public void MyDoubleClickCommand(object param)
         {
@@ -50,6 +62,22 @@ namespace datagrid
         public void PasteExecute()
         {
             MessageBox.Show("Paste");
+        }
+
+        public void AddExecute()
+        {
+            this.Persons.Add(new Person("nnn", 7));
+        }
+
+        public void ReplaceExecute()
+        {
+            var l_t = this.myList.First();
+            l_t.Persons = new ObservableCollection<Person>()
+            {
+                new Person("hhhh", 9),
+                new Person("hhhh", 9),
+                new Person("hhhh", 9),
+            };
         }
     }
 }
